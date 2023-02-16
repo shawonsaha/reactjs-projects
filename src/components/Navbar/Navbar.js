645472465d7f2c95730b16d0ed4ac4dfa38579db
Shawon Saha <shawondroid@gmail.com>
2023-02-16 17:15:47 +0600

    Material UI Dashboard

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,55 @@
+import React from 'react'
+// import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+// import CssBaseline from '@mui/material/CssBaseline';
+// import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+// import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+// import InboxIcon from '@mui/icons-material/MoveToInbox';
+// import MailIcon from '@mui/icons-material/Mail';
+
+import { menuNavbarItems } from './const/NavbarItems';
+
+const drawerWidth = 240;
+
+const Navbar = () => {
+  return (
+    <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+            background: '#101F33',
+            color: 'white'
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar />
+        <Divider />
+        <List>
+          {menuNavbarItems.map((text, index) => (
+            <ListItem key={text.id} disablePadding>
+              <ListItemButton>
+                <ListItemIcon sx={{color:'white'}}>
+                  {text.icon}
+                </ListItemIcon>
+                <ListItemText primary={text.lable} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+  )
+}
+
+export default Navbar
---
+++ src/components/Navbar/const/NavbarItems.js
@@ -0,0 +1,15 @@
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+import StorageIcon from '@mui/icons-material/Storage';
+import CloudDoneIcon from '@mui/icons-material/CloudDone';
+import GiteIcon from '@mui/icons-material/Gite';
+import FunctionsIcon from '@mui/icons-material/Functions';
+import PrecisionManufacturingIcon from '@mui/icons-material/PrecisionManufacturing';
+
+export const menuNavbarItems = [
+    {id: 0, icon: <AdminPanelSettingsIcon/>, lable: 'Authentication', route: 'route'},
+    {id: 2, icon: <StorageIcon/>, lable: 'Database', route: 'route'},
+    {id: 3, icon: <CloudDoneIcon/>, lable: 'Storage', route: 'route'},
+    {id: 4, icon: <GiteIcon/>, lable: 'Hosting', route: 'route'},
+    {id: 5, icon: <FunctionsIcon/>, lable: 'Function', route: 'route'},
+    {id: 6, icon: <PrecisionManufacturingIcon/>, lable: 'Matchine Learning', route: 'route'},
+]
