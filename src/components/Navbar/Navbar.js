0ed2324bf2a41ff0af64624afdc46107308146f9
shawonsaha <shawondroid@gmail.com>
2023-02-17 22:35:57 +0600

    On my way to set up route

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -14,7 +14,51 @@ import ListItemText from '@mui/material/ListItemText';
 // import InboxIcon from '@mui/icons-material/MoveToInbox';
 // import MailIcon from '@mui/icons-material/Mail';
 
+// import App from '../../App';
+// import Authentication from '../Pages/Authentication/Authentication'
+// import Database from '../Pages/Database/Database'
+// import Storage from '../Pages/Storage/Storage'
+// import Hosting from '../Pages/Hosting/Hosting'
+// import Function from '../Pages/Function/Function'
+// import MachineLearning from '../Pages/Machine-Learning/MachineLearning'
+
 import { menuNavbarItems } from './const/NavbarItems';
+
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App/>,
+//   },
+//   {
+//     path: "/authentication",
+//     element: <Authentication/>,
+//   },
+//   {
+//     path: "/database",
+//     element: <Database/>,
+//   },
+//   {
+//     path: "/storage",
+//     element: <Storage/>,
+//   },
+//   {
+//     path: "/hosting",
+//     element: <Hosting/>,
+//   },
+//   {
+//     path: "/function",
+//     element: <Function/>,
+//   },
+//   {
+//     path: "/machine-learning",
+//     element: <MachineLearning/>,
+//   },
+// ]);
 
 const drawerWidth = 240;
 
